92ab22e159624ad5c90f3a0b4fe1031e8e06f548
Pratyush Das <reikdas@gmail.com>
2020-02-01 22:11:10 +0530

    Update naive implementation with template

--- testgsoc/test.cu
+++ testgsoc/test.cu
@@ -2,19 +2,17 @@
 #include "device_launch_parameters.h"
 #include <iostream>
 
+template <typename C, typename T>
 __global__
-void awkward_listarray_compact_offsets(int* tooffsets, const int* fromstarts, const int* fromstops, int64_t startsoffset, int64_t stopsoffset, int64_t length) {
+void awkward_listarray_compact_offsets(T* tooffsets, const C* fromstarts, const C* fromstops, int64_t startsoffset, int64_t stopsoffset, int64_t length) {
 	tooffsets[0] = 0;
 	int i = threadIdx.x + (blockIdx.x * blockDim.x);
 	if (i == 0) {
 		for (int idx = 0; idx < length; idx++) {
-			int start = fromstarts[startsoffset + idx];
-			int stop = fromstops[stopsoffset + idx];
-			if (stop < start) {
-			}
-			else {
-				tooffsets[idx + 1] = tooffsets[idx] + (stop - start);
-			}
+			C start = fromstarts[startsoffset + idx];
+			C stop = fromstops[stopsoffset + idx];
+			assert(start < stop);
+			tooffsets[idx + 1] = tooffsets[idx] + (stop - start);
 		}
 	}
 	__syncthreads();
@@ -31,7 +29,7 @@ int main() {
 	cudaMemcpy(d_fromstarts, fromstarts, 5 * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMalloc((void**)&d_fromstops, 5 * sizeof(int));
 	cudaMemcpy(d_fromstops, fromstops, 5 * sizeof(int), cudaMemcpyHostToDevice);
-	awkward_listarray_compact_offsets <<<1, 5>>> (d_tooffsets, d_fromstarts, d_fromstops, 0, 0, 5);
+	awkward_listarray_compact_offsets <int, int><<<1, 5>>> (d_tooffsets, d_fromstarts, d_fromstops, 0, 0, 5);
 	cudaDeviceSynchronize();
 	cudaMemcpy(tooffsets, d_tooffsets, 6 * sizeof(int), cudaMemcpyDeviceToHost);
 	cudaFree(d_tooffsets);
